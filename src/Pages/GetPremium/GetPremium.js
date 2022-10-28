6c8eddcac393ce7a226f0de0a5d837ae99f586ee
Iftekharshovon01 <iftekharshovonseu17@gmail.com>
2022-10-28 19:11:25 +0600

    Gate Prmium  page update

--- src/Pages/GetPremium/GetPremium.js
+++ src/Pages/GetPremium/GetPremium.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GetPremium = () => {
     return (
         <div>
-            
+            <h1>Congratulation You have get premium access!</h1>
         </div>
     );
 };
