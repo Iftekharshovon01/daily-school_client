802e43ee94b422169f27a9df9206459806109310
Iftekharshovon01 <iftekharshovonseu17@gmail.com>
2022-10-28 21:32:28 +0600

    pdf download done

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -2,20 +2,26 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link, useLoaderData } from 'react-router-dom';
+import Pdf from "react-to-pdf";
 
 const Course = () => {
     const course = useLoaderData()
-    const {name, details, image} = course;
+    const { name, details, image } = course;
+    const ref = React.createRef();
     return (
         <div className='d-flex justify-content-center'>
-             <Card style={{width: "400px", height:"300px"}}>
-                <Card.Img style={{height:"300px"}} variant="top" src={image} />
+
+            <Card ref={ref} style={{ width: "400px", height: "300px" }}>
+                <Card.Img style={{ height: "300px" }} variant="top" src={image} />
                 <Card.Body className='text-center' >
                     <Card.Title>{name}</Card.Title>
                     <Card.Text>
-                    {details}
+                        {details}
                     </Card.Text>
                     <Link className='d-flex justify-content-center' to={`/premium`}><Button variant="primary">Get Premium</Button></Link>
+                    <Pdf targetRef={ref} filename="codurse.pdf">
+                        {({ toPdf }) => <button  onClick={toPdf}>Download Pdf</button>}
+                    </Pdf>
                 </Card.Body>
             </Card>
         </div>
